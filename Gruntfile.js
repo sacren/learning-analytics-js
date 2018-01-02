5a7c5e5425c1da7d0c84850958644d3b2025f32a
Jεan Sacren <sakiwit@gmail.com>
2018-01-01 17:03:05 -0700

    Remove jshint plug-in for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,9 +80,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-qunit')
-  grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-watch')
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify'])
+  grunt.registerTask('default', ['qunit', 'concat', 'uglify'])
 }
