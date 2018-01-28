c7e31cdd3cfe417225c24c33dbb135c9f370f394
Jεan Sacren <sakiwit@gmail.com>
2018-01-28 01:13:54 -0700

    Add after-date-range implementation

--- src/lesson_overview_after.js
+++ src/lesson_overview_after.js
@@ -22,6 +22,8 @@ jQuery(function ($) {
       var pattern = /overview/i
       var d = {}
       var a = []
+      var b = []
+      var p
 
       d = $.parseJSON(data)
       if (d.length === 0) {
@@ -30,24 +32,34 @@ jQuery(function ($) {
       }
 
       for (var i in d) {
+        var timestamp = d[i]['request_timestamp']
+
+        if (timestamp.substring(0, 11) < '2017-02-01') {
+          continue
+        }
+
         var courseId = parseInt(d[i]['course_id'])
         var uid = parseInt(d[i]['user_id'])
         var match = d[i]['http_request_clean']
 
         if (courseId !== selected) { continue }
 
-        if (pattern.test(match) && !a.some(function (x) { return x === uid })) {
-          a.push(uid)
+        if (!a.some(function (x) { return x === uid })) { a.push(uid) }
+
+        if (pattern.test(match) && !b.some(function (x) { return x === uid })) {
+          b.push(uid)
         }
       }
 
       var n = a.length
+      var m = b.length
+
       if (n === 0) {
         $('form + div').html(function () {
           var openDiv = '<div>'
           var closeDiv = '</div>'
           var line1 = openDiv + course + closeDiv
-          var line2 = openDiv + 'No user visited overview page' + closeDiv
+          var line2 = openDiv + 'After the date range, no student visited overview page.' + closeDiv
 
           return line1 + line2
         })
@@ -55,12 +67,12 @@ jQuery(function ($) {
         return
       }
 
-      if (n === 1) {
+      if (m === 0) {
         $('form + div').html(function () {
           var openDiv = '<div>'
           var closeDiv = '</div>'
           var line1 = openDiv + course + closeDiv
-          var line2 = openDiv + 'One user visited overview page' + closeDiv
+          var line2 = openDiv + 'After the date range, no student visited overview page.' + closeDiv
 
           return line1 + line2
         })
@@ -68,11 +80,26 @@ jQuery(function ($) {
         return
       }
 
+      if (n === m) {
+        $('form + div').html(function () {
+          var openDiv = '<div>'
+          var closeDiv = '</div>'
+          var line1 = openDiv + course + closeDiv
+          var line2 = openDiv + 'After the date range, 100%' + msg + closeDiv
+
+          return line1 + line2
+        })
+
+        return
+      }
+
+      p = m / n * 100
+      msg = m + ' or ' + p.toPrecision(2) + '%' + msg
       $('form + div').html(function () {
         var openDiv = '<div>'
         var closeDiv = '</div>'
         var line1 = openDiv + course + closeDiv
-        var line2 = openDiv + n + msg + closeDiv
+        var line2 = openDiv + 'After the date range, ' + msg + closeDiv
 
         return line1 + line2
       })
