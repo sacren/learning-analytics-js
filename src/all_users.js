098148e457b7f801ec21cf09ed68aecacba21e1f
Jεan Sacren <sakiwit@gmail.com>
2018-05-01 01:57:44 -0600

    Use object literal for code encapsulation

--- src/all_users.js
+++ src/all_users.js
@@ -1,93 +1,101 @@
-jQuery(function ($) {
-  function setForm () {
-    $('form input').val('Show All Users')
-  }
+var analytics = {
+  init: function () {
+    analytics.setDisplayMsg()
+    analytics.setForm()
+    analytics.makeSelection()
+    analytics.submitForm()
+  },
 
-  function setDisplayMsg () {
+  setDisplayMsg: function () {
     var msg = 'Would you like to show the number of all users?'
     $('form + div').html(msg)
-  }
+  },
 
-  $('form').submit(function () {
-    var url = 'https://mediafiles.uvu.edu/lib/extracted.php'
-    var msg = 'Pulling data, please wait...'
+  setForm: function () {
+    $('form input').val('Show All Users')
+  },
 
-    $('form input').prop('disabled', true)
-    $('form + div').html(msg)
-
-    $.post(url, $(this).serialize(), function (data) {
-      var course = $('select option:selected').text().replace(/-/g, ' ')
-      var msg = ' students in total'
-      var selected = parseInt($('select option:selected').val())
-      var d = {}
-      var a = []
-
-      d = $.parseJSON(data)
-      if (d.length === 0) {
-        $('form + div').html('Data error!')
-        return
-      }
-
-      for (var i in d) {
-        var courseId = parseInt(d[i]['course_id'])
-        var uid = parseInt(d[i]['user_id'])
-
-        if (courseId !== selected) { continue }
-
-        if (!a.some(function (x) { return x === uid })) { a.push(uid) }
-      }
-
-      var n = a.length
-      if (n === 0) {
-        $('form + div').html(function () {
-          var openDiv = '<div>'
-          var closeDiv = '</div>'
-          var line1 = openDiv + course + closeDiv
-          var line2 = openDiv + 'No enrollment' + closeDiv
-
-          return line1 + line2
-        })
-
-        return
-      }
-
-      if (n === 1) {
-        $('form + div').html(function () {
-          var openDiv = '<div>'
-          var closeDiv = '</div>'
-          var line1 = openDiv + course + closeDiv
-          var line2 = openDiv + 'One student in total' + closeDiv
-
-          return line1 + line2
-        })
-
-        return
-      }
-
-      $('form + div').html(function () {
-        var openDiv = '<div>'
-        var closeDiv = '</div>'
-        var line1 = openDiv + course + closeDiv
-        var line2 = openDiv + n + msg + closeDiv
-
-        return line1 + line2
-      })
-    }).fail(function () {
-      window.alert('Error: Pulling data!')
+  makeSelection: function () {
+    $('form > select').change(function () {
+      $('form input').prop('disabled', false)
+      analytics.setDisplayMsg()
     })
+  },
 
-    return false
-  })
+  submitForm: function () {
+    $('form').submit(function () {
+      var url = 'https://mediafiles.uvu.edu/lib/extracted.php'
+      var msg = 'Pulling data, please wait...'
 
-  $('form > select').change(function () {
-    $('form input').prop('disabled', false)
-    setDisplayMsg()
-  })
+      $('form input').prop('disabled', true)
+      $('form + div').html(msg)
 
-  function init () {
-    setDisplayMsg()
-    setForm()
+      $.post(url, $(this).serialize(), function (data) {
+        var course = $('select option:selected').text().replace(/-/g, ' ')
+        var msg = ' students in total'
+        var selected = parseInt($('select option:selected').val())
+        var d = {}
+        var a = []
+
+        d = $.parseJSON(data)
+        if (d.length === 0) {
+          $('form + div').html('Data error!')
+          return
+        }
+
+        for (var i in d) {
+          var courseId = parseInt(d[i]['course_id'])
+          var uid = parseInt(d[i]['user_id'])
+
+          if (courseId !== selected) { continue }
+
+          if (!a.some(function (x) { return x === uid })) { a.push(uid) }
+        }
+
+        var n = a.length
+        if (n === 0) {
+          $('form + div').html(function () {
+            var openDiv = '<div>'
+            var closeDiv = '</div>'
+            var line1 = openDiv + course + closeDiv
+            var line2 = openDiv + 'No enrollment' + closeDiv
+
+            return line1 + line2
+          })
+
+          return
+        }
+
+        if (n === 1) {
+          $('form + div').html(function () {
+            var openDiv = '<div>'
+            var closeDiv = '</div>'
+            var line1 = openDiv + course + closeDiv
+            var line2 = openDiv + 'One student in total' + closeDiv
+
+            return line1 + line2
+          })
+
+          return
+        }
+
+        $('form + div').html(function () {
+          var openDiv = '<div>'
+          var closeDiv = '</div>'
+          var line1 = openDiv + course + closeDiv
+          var line2 = openDiv + n + msg + closeDiv
+
+          return line1 + line2
+        })
+      }).fail(function () {
+        window.alert('Error: Pulling data!')
+      })
+
+      return false
+    })
   }
+}
 
-  init()
+window.jQuery(function ($) {
+  analytics.init()
 })
